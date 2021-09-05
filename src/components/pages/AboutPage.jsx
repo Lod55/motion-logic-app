0337694e8e7d7d376547a60d030e303bf271b264
Artyom Lodochnikov <lodkin55@yandex.ru>
2021-09-05 14:25:39 +0500

    feat: Добавить страницу с информацией

---
+++ src/components/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <>
+      <h1>Информационная страница</h1>
+      <p>
+        ИнформацияИнформацияИнформацияИнформацияИнформацияИнформацияИнформацияИнформацияИнформацияИнформация
+      </p>
+      <button type="button">Перейти к поиску </button>
+    </>
+  );
+};
+
+export default AboutPage;
